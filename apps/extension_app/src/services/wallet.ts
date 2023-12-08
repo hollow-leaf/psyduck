b21efbcb5326a8b216d2c8115058aec4daf7aeae
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2023-12-08 15:16:20 +0800

    feat: extensions

---
+++ apps/extension_app/src/services/wallet.ts
@@ -0,0 +1,21 @@
+import { Wallet, ethers } from "ethers";
+
+export async function wallet(){
+    
+    //const wallet = new Wallet(sk)
+    const wallet = new Wallet("")
+
+    console.log(wallet.address)
+    
+    const provider = new ethers.JsonRpcProvider("https://goerli.infura.io/v3/3db317632844470fa86a5a3a8cb7724d")
+
+    const signer = wallet.connect(provider)
+
+    const tx = await signer.sendTransaction({
+        to: '0x3bcAbD66522534657BB86a18d3E550f3166755Fa',
+        value: ethers.parseUnits('0.001', 'ether'),
+    });
+    console.log(tx);
+    alert(tx.hash)
+}
+
