cdfa9c392aee00830d191998ba6290aae86d57a4
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2023-12-11 10:24:22 +0800

    fix: fix some bug amd layout

--- apps/extension_app/src/components/profile.tsx
+++ apps/extension_app/src/components/profile.tsx
@@ -1,14 +1,19 @@
 import { useAccount } from "wagmi"
 import { Balance } from "./balance"
+import { DisconnectButton } from "./connect"
+import { useWeb3Modal } from "@web3modal/wagmi/react"
 
 export function Profile(){
 
-    const { address, isConnecting, isDisconnected } = useAccount()
+    const { address } = useAccount()
+    const { open } = useWeb3Modal()
 
     return(
         <section>
             <p>Your Address: {address}</p>
             <Balance />
+            <DisconnectButton />
+            <button onClick={() => open({ view: 'Networks' })}>Network</button>
         </section>
     )
 }
