68804ec8a81b42b1bf73685f96f7eafd47bc8794
kidneyweak <kidneyweak@gmail.com>
2024-02-17 10:29:48 +0800

    feat(contract): verify method

---
+++ packages/contract/README.md
@@ -0,0 +1,4 @@
+```
+pnpm deploy:token --network opbnb-testnet --verify
+pnpm deploy:nftFactory --network opbnb-testnet --verify
+```
--- packages/contract/contracts/DonateToken.sol
+++ packages/contract/contracts/DonateToken.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract DonateToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("DonateToken", "DT") {
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract DonateToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("PSYToken", "PSY") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
